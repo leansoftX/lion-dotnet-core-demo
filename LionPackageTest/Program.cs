18ead81bcedf80df1cb881344c4bdb9ac1bd250e
vincent wei <zlweicoder@outlook.com>
2019-01-15 16:39:42 +0800

    add test project

---
+++ LionPackageTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using LeansoftX.Lion.Client;
+using System;
+
+namespace LionPackageTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lion Test!");
+            var lionClient = new LionClient();
+            var result = lionClient.BoolVariation("true");
+            Console.WriteLine("Feature Flag Status:" + result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
