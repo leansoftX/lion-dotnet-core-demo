7f2014172a6ec235df4c353308da2dd10e295e7d
振龙 魏 <VincentWei@leansoftx.com>
2019-01-15 18:55:11 +0800

    [FeatureFlag] Status api

--- LionPackageTest/Program.cs
+++ LionPackageTest/Program.cs
@@ -8,8 +8,8 @@ namespace LionPackageTest
         static void Main(string[] args)
         {
             Console.WriteLine("Lion Test!");
-            var lionClient = new LionClient();
-            var result = lionClient.BoolVariation("true");
+            var lionClient = new LionClient("sdk-1fbfa9ca-7bd8-4a42-a328-05d8f924aa60");
+            var result = lionClient.BoolVariation("labs");
             Console.WriteLine("Feature Flag Status:" + result.ToString());
             Console.ReadLine();
         }
